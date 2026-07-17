ff77f704b2350eeeab85a1ba58208c7d3483e2f3
Вениамин Фишман <vsefishman@mkristo-front03.gigalearn.internal>
2026-07-17 11:37:23 +0300

    Track Giga segmentation and finding jobs

---
+++ finding/jobs/job_edge_moderngena_base_plain_shared.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=findembpf3b6a
+#SBATCH --partition=rnd
+
+#SBATCH --account=shared
+#SBATCH --qos=shared-high
+#SBATCH --nodes=1
+#SBATCH --time=480:00:00
+#SBATCH --gpus-per-node=8
+#SBATCH --ntasks-per-node=16
+
+set -eo pipefail
+
+date
+source $HOME/envs/genatator_pipeline/bin/activate
+set -u
+cd $HOME/DNALM/GENATATOR/
+
+export HF_HOME=$HOME/.hf
+export TRANSFORMERS_OFFLINE=1
+export HF_HUB_OFFLINE=1
+export TOKENIZERS_PARALLELISM=0
+export PYTHONPATH=$PWD
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 finding/train.py \
+  --config finding/configs/edge_moderngena_base_plain.json
+
+date
+echo "Done!"
